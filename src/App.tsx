2571ab423d35556b3344032e38f10aff8d9ad857
Sonia Gomez <soniagmz@alumnos.cei.es>
2026-03-03 20:45:32 +0100

    union de llos componentes en App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import PlanCard from './PlanCard'
+import PlanCard from './components/PlanCard/PlanCard'
+import StockBadge from './components/StockBadge/StockBadge'
+import Notifications from './components/Notifications'
 import './App.css'
 
 function App() {
@@ -8,6 +10,16 @@ function App() {
       {/* PLAN */}
       <PlanCard nombrePlan='Familiar' esPopular={true} />
       <PlanCard nombrePlan='Individual' esPopular={false} />
+
+      {/* STOCK BADGE */}
+      <StockBadge productName='camiseta' quantity={0} />
+      <StockBadge productName='pantalones' quantity={5} />
+      <StockBadge productName='abrigo' quantity={15} />
+
+      {/* NOTIFICATIONS */}
+      <Notifications tipo='exito' mensaje='Usuario creado' />
+      <Notifications tipo='alerta' mensaje='No hay gasolina' />
+      <Notifications tipo='error' mensaje='No se pudo guardar en la base de datos' />
     </>
   )
 }
